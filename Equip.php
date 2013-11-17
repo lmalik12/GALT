652b30358fbecf123c51816152da3ee216bb8adb
lmalik12 <lovedeepmalik@gmail.com>
2013-11-17 15:59:50 -0800

    Create Equip.php

---
+++ Equip.php
@@ -0,0 +1,40 @@
+<!-- Tennis Centre -->
+<html>
+	<head>
+		<title> Equipment </title>
+		<link rel="stylesheet" type= "text/css" href="style.css">
+	</head>	
+	<body>
+		<h1 id= "title"> Tennis Center </h1>
+
+		<p>
+			<div id= "Body">
+				Tennis Racket:<select name= dropdown>
+					<option value = "0"> 0 </option>				
+					<option value = "1"> 1 </option>
+					<option value = "2"> 2 </option>
+					<option value = "3"> 3 </option>
+					<option value = "4"> 4 </option>
+				<br/> <br/>
+				Tennis Ball: <select name= dropdown>
+					<option value = "0"> 0 </option>
+					<option value = "1"> 1 </option>
+					<option value = "2"> 2 </option>
+					<option value = "3"> 3 </option>
+					<option value = "4"> 4 </option>
+					<option value = "5"> 5 </option>
+				<br/> <br/>
+				Tennis Ball Machine: <select name= dropdown>
+					<option value = "0"> 0 </option>
+					<option value = "1"> 1 </option>
+					<option value = "2"> 2 </option>
+					<option value = "3"> 3 </option>
+				<br/> <br/>
+				<input type = "submit" value="submit">
+			</div>
+			<br/>
+		</p>	
+	</body>
+</html>
+
+
