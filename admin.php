1f32f76ce96401e2c2ada15ae0b7be68cde74fc5
lmalik12 <lovedeepmalik@gmail.com>
2013-11-17 15:45:43 -0800

    Update admin.php
    Added some html

--- admin.php
+++ admin.php
@@ -1,8 +1,24 @@
+<html>
+        <head>
+                <title> Tennis Center </title>
+                <link rel = "stylesheet" type = "text/css" href = "style.css">
+        </head>        
+        <body>
+            <div id="container">
+                <h1 id = "title"> Tennis Center </h1>
+                <p>
+                    <div id = "sidebarleft">
+                        <p> <a href = "Calendar.php" title="Court"> Make a Reservation </a> </p>
+                        <p> <a href = "CustBooks.php" title="CustBooks"> View Customer Bookings </a> </p> 
+                        <p> <a href = "Equip.php" title="Rent"> Rent Equipment </a> </p> 
+                    </div> 
+                </p> 
+            </div>     
+        </body>
+</html>
+
 <?php
-
-		echo ("<br> Hello ". $_COOKIE["user"].", thank you for signing in.<br>");
-		echo ("<br> You are the admin<br>");
-		echo ("<br> WELCOME TO YOUR OWN SITE OF DATABASES!!");
-
-
+    echo ("<br> Hello ". $_COOKIE["user"].", Thank you for signing in <br>");
+    echo ("<br> You are the admin <br>");
+    echo ("<br> WELCOME TO YOUR OWN SITE OF DATABASES!!");
 ?>
