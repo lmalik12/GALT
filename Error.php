b6b13e2f7198b21023f133a05cff174d8f384c7b
lmalik12 <lovedeepmalik@gmail.com>
2013-11-17 15:43:52 -0800

    Create Error.php
    This is the error page the links the wrong login between re-entering or creating a new account

---
+++ Error.php
@@ -0,0 +1,26 @@
+<html>
+        <head>
+                <title> Tennis Center</title>
+                <link rel="stylesheet" type= "text/css" href="style.css">
+        </head>        
+        <body>
+                <h1 id= "title"> Tennis Center </h1>
+
+                <p>
+                    <div id= "Body">
+                        <form method= "POST" action= "Error.php">
+                            <h1> There was an error logging you in </h1>
+                            <br/> <br/>
+                            <p> Please re-enter your username and password <br/> 
+                                <a href= "TC.php"> Click Here </a>
+                                <br/> <br/> If you are a new customer, create an account 
+                                <br/>
+                            <a href="Account.php"> Register NOW! </a> </p>
+                        </form>
+                    </div>
+                    <br/>
+
+
+                </p>        
+        </body>
+</html>
