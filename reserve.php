e5d9a86a14f9bda6d4cb4f9ad9b3efd08409e404
lmalik12 <lovedeepmalik@gmail.com>
2013-11-17 19:42:20 -0800

    Update reserve.php

--- reserve.php
+++ reserve.php
@@ -11,18 +11,18 @@
 			<div id= "Body">
 			<h1> Make a reservation </h1>
 			</br></br>
-			Date: <input type = "text" placeholder= "dd/mm/yyyy">
-				</br></br>
 			Tennis Center Location:<select name= dropdown>
 					<option value = "1212121212"> 2205 Lower Mall </option>				
 					<option value = "1313131313"> 1904 University Blvd </option>
 					<option value = "1414141414"> 720 Mainland Street </option>
 					<option value = "1515151515"> 101 East Broadway </option>
-					<option value = "1616161616"> 721 West Broadway </option>
+					<option value = "1616161616"> 721 West Broadway </option> </select>
 				</br> </br>
 			Type of Court: <select name = dropdown>
 					<option value = "Indoor"> Indoor </option>
-					<option value = "Outdoor"> Outdoor </option>
+					<option value = "Outdoor"> Outdoor </option> </select>
+				</br></br>
+			Date: <input type = "text" placeholder= "dd/mm/yyyy">
 				</br></br>
 			Timeslot: <select name = dropdown> <!-- Open from 10am-6pm -->
 				<option value = "10:00/11:00pm"> 10:00am - 11:00am </option>
@@ -32,7 +32,7 @@
 				<option value = "14:00/15:00pm"> 2:00pm - 3:00pm </option>
 				<option value = "15:00/16:00pm"> 3:00pm - 4:00pm </option>
 				<option value = "16:00/17:00pm"> 4:00pm - 5:00pm </option>
-				<option value = "17:00/18:00pm"> 5:00pm - 6:00pm </option>
+				<option value = "17:00/18:00pm"> 5:00pm - 6:00pm </option> </select>
 			</br></br>
 				<input type = "submit" value= "submit" name="newReserve">
 			</div>
