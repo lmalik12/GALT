335eb3a9bb4ee557f7ba692b0b8d1511729e2b21
Gabrielle <gabrielle.chen@hotmail.com>
2013-11-17 16:12:05 -0800

    added queries and data

--- data.sql
+++ data.sql
@@ -34,28 +34,28 @@ Create table reservation (
      cusID varchar2(15),
      TID varchar2(10),
      PRIMARY KEY (confirNum),
-     FOREIGN KEY (TID) references tennis_centre,
-     FOREIGN KEY (cusID) references customer); 
+     FOREIGN KEY (TID) references tennis_centre ON DELETE CASCADE, 
+     FOREIGN KEY (cusID) references customer ON DELETE CASCADE); 
 
 Create table admin (
     adminID varchar2(15) not null,
     TID varchar2(10) not null,
 PRIMARY KEY (adminID, TID),
-FOREIGN KEY (TID) references tennis_centre);
+FOREIGN KEY (TID) references tennis_centre ON DELETE CASCADE);
 
 Create table court (
     courtID CHAR(8),
     court_type CHAR(7),
     TID varchar2(10),
 PRIMARY KEY (courtID, TID),
-FOREIGN KEY (TID) references tennis_centre);
+FOREIGN KEY (TID) references tennis_centre ON DELETE CASCADE);
 
 Create table equipment (
     EID CHAR(10) not null,
     type varchar2(10),
     confirNum CHAR(10),
 PRIMARY KEY (EID),
-FOREIGN KEY (confirNum) references reservation);
+FOREIGN KEY (confirNum) references reservation ON DELETE CASCADE);
 
 --cusID, name, phone, address
 insert into customer values('woodie', 'Woodie', '778-333-2222', '7482 EDWARD STREET');
@@ -63,6 +63,8 @@ insert into customer values('lovedeep','LoveDeep', '604-222-2222', '37 SHELL AVE
 insert into customer values('taranbir', 'Taranbir', '604-444-4444', '3829 MADISON STREET');
 insert into customer values('arwud', 'Arwud', '778-381-8233', '111 DATA AVENUE');
 insert into customer values('rachel', 'Rachel', '604-003-3913', '39 BLUE ROAD');
+--no reservation customer
+insert into customer values('rochelle', 'Rochelle', '604-003-3913', '39 BLUE ROAD');
 
 --INSIDE LOGIN, CREATE FIELD ptype INT FOR [1-ADMIN][0-CUSTOMER]
 --login for admin
@@ -80,6 +82,7 @@ insert into login values ('lovedeep', '777', 0);
 insert into login values ('taranbir', '888', 0);
 insert into login values ('arwud', '999', 0);
 insert into login values ('rachel ', '000', 0);
+insert into login values ('rochelle', '191', 0);
 
 --TID, address, phone
 insert into tennis_centre values ('1212121212', '2205 LOWER MALL', '604-555-6666');
@@ -88,13 +91,14 @@ insert into tennis_centre values ('1414141414', '720 MAINLAND STREET', '604-777-
 insert into tennis_centre values ('1515151515', '101 EAST BROADWAY', '604-888-9999');
 insert into tennis_centre values ('1616161616', '721 WEST BROADWAY', '604-999-0101');
 
---confirNum, dated, timeslot, payment, court_slot, cusID, TID
+--confirNum, dated, timeslot, payment, court_type, cusID, TID
 insert into reservation values('1111111111', '01/01/2001', '12:00/13:00', '20', 'IN-DOOR','woodie', '1212121212');
-insert into reservation values('2222222222', '02/02/2002', '13:00/14:00', '20', 'IN-DOOR','lovedeep', '1313131313');
-insert into reservation values('3333333333', '03/03/2003', '14:30/16:00', '30', 'OUTDOOR','taranbir', '1414141414');
+insert into reservation values('2222222222', '02/02/2002', '13:00/14:00', '20', 'OUTDOOR','lovedeep', '1313131313');
+insert into reservation values('3333333333', '03/03/2003', '14:30/16:00', '30', 'IN-DOOR','taranbir', '1414141414');
 insert into reservation values('4444444444', '04/04/2004', '17:00/18:00', '20', 'OUTDOOR','arwud', '1515151515'); 
-insert into reservation values('5555555555', '05/05/2005', '19:00/21:00', '40', 'OUTDOOR','rachel', '1616161616');
---insert into reservation values('5555555555', '05/06/2013', '14:00/15:30', '50', 'INDOOR','rachel');
+--rachel has 2 reservation in outdoor and indoor court and 2 equipment types (same tennis centre)
+insert into reservation values('5555555555', '05/05/2013', '19:00/21:00', '40', 'OUTDOOR','rachel', '1616161616');
+insert into reservation values('6666666666', '05/06/2013', '14:00/15:30', '50', 'INDOOR', 'rachel', '1616161616');
 
 --adminID, TID
 insert into admin values('gabrielle','1212121212');
@@ -109,6 +113,9 @@ insert into court values('45889032', 'OUTDOOR','1313131313');
 insert into court values('22873987', 'IN-DOOR','1414141414');
 insert into court values('10092766', 'OUTDOOR','1515151515');
 insert into court values('77890374', 'OUTDOOR','1616161616');
+insert into court values('77812382', 'IN-DOOR','1616161616');
+--PROBLEM: courtID and TID are primary key so you can't have duplicates but what if a customer makes a reservation 
+--at the same tennis centre and court
 
 --EID, type, confirNum
 insert into equipment values ('122','BALL','1111111111');
@@ -116,6 +123,6 @@ insert into equipment values ('225','RACKET','2222222222');
 insert into equipment values ('3123', 'TBM','3333333333');
 insert into equipment values ('447', 'BALL','4444444444');
 insert into equipment values ('3789', 'BALL','5555555555');
---insert into equipment values ('3788', 'RACKET','5555555555');
+insert into equipment values ('3788', 'RACKET','6666666666');
 
 
--- queries.php
+++ queries.php
@@ -50,12 +50,12 @@ where c.TID=tc.TID
 group by c.court_type
 having c.court_type='OUTDOOR';
 
-8) --count how many courts are available (indoor court and outdoor)
+8) count how many courts are available (indoor court and outdoor)
 select count(*)
 from reservation r, court c, customer cu, tennis_centre tc
 where cu.cusID=r.cusID and r.TID=tc.TID and c.TID=tc.TID;
 
-9) --how many indoor courts are available
+9) how many indoor courts are available
 select count(*)
 from reservation r, court c, customer cu, tennis_centre tc
 where cu.cusID=r.cusID and r.TID=tc.TID and c.TID=tc.TID
@@ -74,14 +74,14 @@ select tc.address
 from reservation r, court c, customer cu, tennis_centre tc
 where cu.cusID=r.cusID and r.TID=tc.TID and c.TID=tc.TID and r.timeslot='12:00/13:00';
 
->>>13) 
+>>>13) DIVISION QUERY
 
-TRY>>>14) What is the schedule for a court?
+14) What is the schedule for a court?
 select r.timeslot
 from reservation r, court c, customer cu, tennis_centre tc
 where cu.cusID=r.cusID and r.TID=tc.TID and c.TID=tc.TID and c.courtID='22873987';
 
-?>>>>>15) Has a customer paid for their reservation?
+>>>>>15) Has a customer paid for their reservation?
 
 16) How many customers are playing in this court?
 select count(*)
@@ -94,21 +94,22 @@ select *
 from reservation r, customer c
 where c.cusID = 'lovedeep' and c.cusID=r.cusID;
 
-18)--show all reservations for customers
+18) show all reservations for customers
 (or select *)
 select c.name, c.phone, c.address, r.dated, r.timeslot, r.payment, r.court_type
 from reservation r, customer c
 where r.cusID=c.cusID
 order by c.name;
 
-TRY>>19) What courts are currently available?
+DC>>19) What courts are currently available?
 select c1.courtID
 from court c1
 where c1.courtID not in (select c.courtID
                         from reservation r, court c, customer cu, tennis_centre tc
                         where cu.cusID=r.cusID and r.TID=tc.TID and c.TID=tc.TID);
 
-TRY>>20) What courts are currently available at specific tennis centre?
+DC>>20) What courts are currently available at specific tennis centre?
+--returned courts not in lower mall tennis centre
 select c1.courtID
 from court c1, tennis_centre tc1
 where c1.TID=tc1.TID and c1.courtID not in (select c.courtID
@@ -149,14 +150,52 @@ select r.timeslot
 from reservation r, customer c
 where r.cusID=c.cusID and c.cusID='rachel';
 
-26) What equipment have my booked?
+26) What equipment have I booked?
 --what is the reserved equipment for a reservation for specific customer
 select e.EID, e.type 
 from customer c, equipment e, reservation r
 where c.cusID=r.cusID and r.confirNum=e.confirNum and c.cusID='lovedeep';
 
-27)--show all reserved equipment by customers
+27) show all reserved equipment by customers
 select e.EID, e.type 
 from customer c, equipment e, reservation r
 where c.cusID=r.cusID and r.confirNum=e.confirNum;
 
+?PARENS>>28) find the names of customers who reserved specific court
+select DISTINCT cu.name
+from customer cu, reservation r, tennis_centre tc, court c
+where cu.cusID=r.cusID and r.TID=tc.TID and tc.TID=c.TID and c.courtID='22873987';
+
+>>select DISTINCT cu.name
+from customer cu
+where cu.cusID in (select r.cusID
+                   from reservation r, tennis_centre tc, court c
+                   where r.TID=tc.TID and tc.TID=c.TID and c.courtID='22873987');
+
+???>>29) find the names of customers who reserved an indoor and outdoor court
+select DISTINCT cu.name
+from customer cu, reservation r, tennis_centre tc, court c
+where cu.cusID=r.cusID and r.TID=tc.TID and tc.TID=c.TID and c.court_type='OUTDOOR'
+INTERSECT
+select DISTINCT cu.name
+from customer cu, reservation r, tennis_centre tc, court c
+where cu.cusID=r.cusID and r.TID=tc.TID and tc.TID=c.TID and c.court_type='IN-DOOR';
+
+??PARENS>>30) find the names of customers who have not reserved a court
+select DISTINCT cu.name
+from customer cu
+where cu.cusID not in (select r.cusID
+					   from reservation r
+					   where r.TID in (select tc.TID
+					   				   from tennis_centre tc
+					   				   where tc.TID in (select c.TID
+					   				   					from court c);
+
+31) TRIGGER: when user creates new account (PHP), insert into customer table
+32) TRIGGER: when user cancels reservation (PHP), delete reservation tuple (not customer tuple)		
+33) TRIGGER reservation - tennis centre info updated
+34) TRIGGER reservation - customer info updated
+35) TRIGGER admin - tennis centre info updated
+36) TRIGGER	court - tennis centre info updated
+37) TRIGGER equipment - reservation info updated		   				   					
+					   				   					
